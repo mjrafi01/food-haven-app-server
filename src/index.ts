de2c79a01531844d3b15cbfbe96b924835dc151e
unknown <dreamboyrafi16@gmail.com>
2024-06-21 14:24:56 +0800

    stripe added

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
 import restaurantRoute from "./routes/RestaurantRoute";
+import orderRoute from "./routes/OrderRoute";
 import {v2 as cloudinary} from "cloudinary";
+
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to database"))
@@ -18,6 +20,8 @@ mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
     api_secret: process.env.CLOUDINARY_API_SECRET
   })
 const app = express();
+
+app.use("/api/order/checkout/webhook", express.raw({ type: "*/*" }));
 app.use(express.json());
 app.use(cors());
 
@@ -30,6 +34,7 @@ app.get("/health",async (req:Request, res:Response) => {
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant",myRestaurantRoute);
 app.use("/api/restaurant",restaurantRoute);
+app.use("/api/order",orderRoute);
 
 app.listen(7000, () => {
   console.log("Server started on localhost:7000");
--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -6,6 +6,11 @@ import RestaurantController from "../controllers/RestaurantController"
 
 const router= express.Router()
 
+
+router.get("/:restaurantId",param("restuarantId").isString().trim().notEmpty()
+.withMessage("City param must be a String"),RestaurantController.getRestaurant)
+
+
 router.get("/search/:city",param("city").isString().trim().notEmpty().withMessage("City param must be a String"),
 RestaurantController.searchRestaurant
 )
